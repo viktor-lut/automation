b36f2bd9163807a9a2b0cd28c5cae8bc812e07fe
Marikopy <marikopy83@gmail.com>
2019-01-07 08:09:05 -0500

    Marina commit

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ describe('Page opening', function () {
     let title = browser.getTitle(); //get page title and assign it to the "title" variable
     browser.pause(5000); //just pause to visually see that something is happening on the page
     console.log(title); //log "title" variable
+    const a = 1;
     assert.equal(title, 'Bug Tracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
   })
 });
