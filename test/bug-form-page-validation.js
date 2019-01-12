d6bca69fc3a52797f19a41fcfa10f50756efe00c
EgorSundukou <44594730+EgorSundukou@users.noreply.github.com>
2019-01-11 20:35:57 -0800

    conflikt

--- test/bug-form-page-validation.js
+++ test/bug-form-page-validation.js
@@ -4,11 +4,14 @@ describe('Bug-form-page-validation', function () {
 
     it('Text: “Please fill all the required fields”', function(){
         browser.url('/');
-        browser.pause(500);
+        browser.pause(3000);
         browser.setValue('#email', '6940563@gmail.com');
         browser.setValue('#pass', '123');
         browser.click('#login');
+        //browser.pause(8000);
+        browser.waitForVisible('//*[@id="root"]/div/div[2]/div/div[2]/table/tbody/tr[1]/th/span');
         browser.click('//*[@id="root"]/div/div[2]/div/div[2]/table/tbody/tr[1]/th/span');
+        browser.pause(3000);
 
 
 
