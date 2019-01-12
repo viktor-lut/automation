bdca1487b9f6f254fdcde064e4e6256b1f68d087
Yuliya Zavarskaya <yulia@Yuliyas-MacBook-Air.local>
2019-01-11 17:13:18 -0800

    added one test case in 'Registration page components' sub-suite plus two new sub-suites: 'First Name field' and 'Last Name field'

--- test/registration-design.js
+++ test/registration-design.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 
 describe('Registration page components', function () {
 
-    it('registration button is clicked, register button and back button exist', function(){
+    it('registration button is clicked, registration page has register button and back button', function(){
         browser.url('/');
-        let registrationButtonExists = browser.waitForVisible('#registration', 3000);
+        let registrationButtonExists = browser.waitForVisible('#registration', 7000);
         assert.equal(registrationButtonExists, true, 'Registration button is not found');
         browser.click('#registration');
-        let registerButtonExists = browser.waitForVisible('button=Register', 1000);
+        let registerButtonExists = browser.waitForVisible('button=Register', 3000);
         assert.equal(registerButtonExists, true, 'Registration page does not contain Register button');
         let backButtonExists = browser.isVisible('button=< Back');
         assert.equal(backButtonExists, true, 'Back button is not found');
@@ -15,12 +15,61 @@ describe('Registration page components', function () {
 
     it('text fields (first name, last name, email, confirm email, password, confirm password) exist', function(){
         let allInputs = $$('.input-group input')
-        let allInputsList = allInputs.map(function(el) { return el.getAttribute('aria-label'); })
-        console.log(allInputsList.join(','));
-        assert.equal(allInputsList.join(', '), 'First name, Last name, Email, Confirm Email, Password, Confirm Password', 'At least one text field does not exist');
+        let allInputsArr = allInputs.map(function(el) { return el.getAttribute('aria-label'); })
+        assert.equal(allInputsArr.join(', '), 'First name, Last name, Email, Confirm Email, Password, Confirm Password', 'At least one text field does not exist');
     })
 
+    it('“* Required field” text exist', function(){
+        let text = browser.getText('p=* Required field');
+        assert.equal(text, '* Required field', 'Text does not exist');
+    })
 
+});
 
+describe('First Name field', function () {
+
+    it('placeholder name', function(){
+        let placeholder = browser.getAttribute('#fname', 'placeholder');
+        assert.equal(placeholder, 'First Name *', 'placeholder name is incorrect');
+    })
+
+    it('input design', function(){
+        const sel = '#fname';
+        const elem = $(sel);
+        let size = elem.getCssProperty('font-size')['value'];
+        assert.equal(size, '16px', 'font-size is incorrect');
+        let weight = elem.getCssProperty('font-weight')['value'];
+        assert.equal(weight, '400', 'font-weight is incorrect');
+        let font_family = elem.getCssProperty('font-family')['value'];
+        assert.equal(font_family, 'segoe ui', 'font-family is incorrect');
+        let maxlength = browser.getAttribute(sel, 'maxlength');
+        assert.equal(maxlength, '20', 'maxlength is incorrect');
+        //let placeholder_color = elem.getCssProperty('::placeholder'); //['style'];
+        //console.log(placeholder_color);
+        //assert.equal(placeholder_color, '#6c757d', 'placeholder color is incorrect');
+    })
+
+});
+
+describe('Last Name field', function () {
+
+    it('placeholder name', function(){
+        let placeholder = browser.getAttribute('#lname', 'placeholder');
+        assert.equal(placeholder, 'Last Name *', 'placeholder name is incorrect');
+    })
+
+    it('input design', function(){
+        const sel = '#lname';
+        const elem = $(sel);
+        let size = elem.getCssProperty('font-size')['value'];
+        assert.equal(size, '16px', 'font-size is incorrect');
+        let weight = elem.getCssProperty('font-weight')['value'];
+        assert.equal(weight, '400', 'font-weight is incorrect');
+        let font_family = elem.getCssProperty('font-family')['value'];
+        assert.equal(font_family, 'segoe ui', 'font-family is incorrect');
+        let maxlength = browser.getAttribute(sel, 'maxlength');
+        console.log(maxlength);
+        assert.equal(maxlength, '20', 'maxlength is incorrect');
+    })
 
 });
