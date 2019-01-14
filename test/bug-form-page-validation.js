267994ccd1b311971fca01bfa9f70448ac33d706
EgorSundukou <44594730+EgorSundukou@users.noreply.github.com>
2019-01-14 14:33:54 -0800

    new branch

--- test/bug-form-page-validation.js
+++ test/bug-form-page-validation.js
@@ -4,18 +4,20 @@ describe('Bug-form-page-validation', function () {
 
     it('Text: “Please fill all the required fields”', function(){
         browser.url('/');
-        browser.pause(3000);
+        browser.waitForVisible('#email');
         browser.setValue('#email', '6940563@gmail.com');
         browser.setValue('#pass', '123');
         browser.click('#login');
-        //browser.pause(8000);
         browser.waitForVisible('//*[@id="root"]/div/div[2]/div/div[2]/table/tbody/tr[1]/th/span');
         browser.click('//*[@id="root"]/div/div[2]/div/div[2]/table/tbody/tr[1]/th/span');
+        //  assert.equal(title, 'Bug Tracker', 'Title is incorrect');
+
+    })
+    it('Verify that  Border color: #f5c6cb', function(){
         browser.pause(3000);
-
-
-
-      //  assert.equal(title, 'Bug Tracker', 'Title is incorrect');
+        let MessageLocator = '#root > div > div.site-content > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(21) > div';
+        let border = $(MessageLocator).getCssProperty('border-bottom-color');
+        //  assert.equal(title, 'Bug Tracker', 'Title is incorrect');
 
     })
 
