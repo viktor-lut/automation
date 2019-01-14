99e796a1d9faa4619fd39fe143c3877568eeb9dd
Yuliya Zavarskaya <yulia@Yuliyas-MacBook-Air.local>
2019-01-13 22:50:52 -0800

    added function getCssPropValue(cssProp, selector) plus refactored two sub-suites by using common function

--- test/registration-design.js
+++ test/registration-design.js
@@ -1,36 +1,52 @@
+function getCssPropValue(cssProp, selector) {
+    let value = '';
+    for (let i = 0; i < cssProp.length; i++) {
+        value = $(selector).getCssProperty(cssProp[i][0]);
+        console.log(value);
+        value = cssProp[i][0].includes('color') ? value['parsed']['hex'] : value['value'];
+        assert.equal(value, cssProp[i][1], 'expected ' + cssProp[i][0] + ' is ' + cssProp[i][1] + ', got ' + value);
+    }
+}
+
 const assert = require('assert');
 
 describe('Registration page components', function () {
 
-    it('registration button is clicked, registration page has register button and back button', function(){
+    it('all required buttons are exist and their design matches spec', function(){
         browser.url('/');
-        let registrationButtonExists = browser.waitForVisible('#registration', 7000);
-        assert.equal(registrationButtonExists, true, 'Registration button is not found');
+        browser.waitForVisible('#registration', 10000);
         browser.click('#registration');
-        let registerButtonExists = browser.waitForVisible('button=Register', 3000);
+        /*let registerButtonExists = browser.waitForVisible('button=Register', 10000);
         assert.equal(registerButtonExists, true, 'Registration page does not contain Register button');
         let backButtonExists = browser.isVisible('button=< Back');
         assert.equal(backButtonExists, true, 'Back button is not found');
+        browser.waitForVisible('button=Register', 10000);*/
+        let allButtons = $$('.input-group-append button');
+        const btnNames = allButtons.map(function(el) { return el.getText(); });
+        const expBtnNames = ['< Back', 'Register'];
+        const cssProp = [['font-size', '16px'], ['font-weight', '400'],
+                            ['font-family', 'segoe ui'], ['color', '#ffffff'], ['background-color', '#17a2b8']];
+        for (let i = 0; i < btnNames.length; i++) {
+            assert.equal(btnNames[i], expBtnNames[i], 'expected button name is ' + expBtnNames[i] + ', got ' + btnNames[i]);
+            getCssPropValue(cssProp, 'button=' + btnNames[i]);
+        }
     });
 
-    it('all text fields are exist and their design matches spec', function(){
-        let allInputs = $$('.input-group input')
-        let PlholdersAndMaxL = allInputs.map(function(el) { return [el.getAttribute('placeholder'), el.getAttribute('maxlength')]; });
-        const selectors = ['#fname', '#lname', '#email', '#email_confirm', '#pass', '#pass_confirm'];
-        const expCssProp = [['font-size', '16px'], ['font-weight', '400'],
+    it('all required text fields are exist and their design matches spec', function(){
+        const allFields = $$('.input-group input');
+        const fieldSelectors = allFields.map(function(el) { return '#' + el.getAttribute('id'); });
+        const plholderProp = allFields.map(function(el) { return [el.getAttribute('placeholder'), el.getAttribute('maxlength')]; });
+        const cssProp = [['font-size', '16px'], ['font-weight', '400'],
                             ['font-family', 'segoe ui'], ['color', '#495057']];
-        const expPlholdersAndMaxL = [['First Name *', '20'], ['Last Name *', '20'],
+        const expPlholderProp = [['First Name *', '20'], ['Last Name *', '20'],
                                 ['Email *', '45'], ['Confirm Email *', '45'],
                                 ['Password *', '45'], ['Confirm Password *', '45']];
-        let cssPropVal = '';
-        for (let i = 0; i < selectors.length; i++) {
-            assert.equal(PlholdersAndMaxL[i][0], expPlholdersAndMaxL[i][0], expPlholdersAndMaxL[i][0] + 'placeholder is incorrect');
-            assert.equal(PlholdersAndMaxL[i][1], expPlholdersAndMaxL[i][1], 'max length of ' + expPlholdersAndMaxL[i][0] + ' field is incorrect');
-            for (let j = 0; j < expCssProp.length; j++) {
-                cssPropVal = $(selectors[i]).getCssProperty(expCssProp[j][0]);
-                cssPropVal = expCssProp[j][0] === 'color'? cssPropVal['parsed']['hex'] : cssPropVal['value'];
-                assert.equal(cssPropVal, expCssProp[j][1], expCssProp[j][0] + ' is incorrect');
-            }
+        for (let i = 0; i < fieldSelectors.length; i++) {
+            assert.equal(plholderProp[i][0], expPlholderProp[i][0], expPlholderProp[i][0] + 'placeholder is incorrect');
+            assert.equal(plholderProp[i][1], expPlholderProp[i][1], 'expected max length of ' + expPlholderProp[i][0] +
+                                                                    'field is ' + expPlholderProp[i][1] +
+                                                                    ', got ' + plholderProp[i][1]);
+            getCssPropValue(cssProp, fieldSelectors[i]);
         }
     });
 
