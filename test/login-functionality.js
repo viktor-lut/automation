f70773fc9f39037a6483ae24787d9669ad8f0703
ViktoriaKomlik <vica93v@gmail.com>
2019-01-15 17:06:28 -0800

    added 2 test cases

--- test/login-functionality.js
+++ test/login-functionality.js
@@ -1,6 +1,19 @@
 const assert = require('assert');
 
 describe('LoginFunctionality', function () {
+
+    it('Text - Specify email and password - appears when trying to login with empty login field', function(){
+        browser.url('/');
+        browser.waitForVisible('#login', 5000);
+        const elem = $('#email');
+        const value = elem.getValue();
+        if (value === '') {
+            browser.click('#login')
+        }
+        let result = browser.waitForVisible('.alert-danger', 5000);
+        assert.equal(result, true, 'The text doesn/t appear when field email is empty');
+    })
+
     it('Field Email accepts 1 symbols', function(){
         browser.url('/');
         browser.waitForVisible('#login', 5000);
@@ -20,43 +33,77 @@ describe('LoginFunctionality', function () {
     it('Field Email doesn/t accepts 46 symbols', function(){
         let email = $('#email');
         email.addValue('1');
-        console.log(email.addValue('1'))
+       // console.log(email.addValue('1'))
         let value = email.getValue();
-        console.log(value);
+        //console.log(value);
         assert.equal('0', '0', 'Email  doesn/t accept 46 symbols');
     })
 
-  it('error design css properties', function(){
-    browser.url('/');
-    browser.waitForVisible('#login', 5000);
-    browser.click('#login');
-    browser.waitForVisible('.alert-local', 5000);
-    let elem = $('.alert-local');
-    let font_family = elem.getCssProperty('font-family')['value'];
-    assert.equal(font_family, 'segoe ui', 'Button has incorrect font-family');
-    let background_color = elem.getCssProperty('background-color')['parsed']['hex'];
-    assert.equal(background_color, '#f8d7da', 'Button has incorrect background color');
-    let borderColorProperties = elem.getCssProperty('border');
-    let borderValue = borderColorProperties.value;
-    let border_color = borderValue.substring(borderValue.indexOf('('));
-    assert.equal(border_color, '(245, 198, 203)', 'Button has incorrect border color');
-    let fontSize = elem.getCssProperty('font-size');
-    assert.equal(fontSize.value, '16px', 'Button has incorrect font-size');
-    let fontWeight = elem.getCssProperty('font-weight');
-    assert.equal(fontWeight.value, '400', 'Button has incorrect font-weight');
-    let elem2 = $('.alert-danger');
-    let color = elem.getCssProperty('color');
-    assert.equal(color.parsed.hex, '#721c24', 'Button has incorrect color');
-    let textAlign = elem.getCssProperty('text-align');
-    assert.equal(textAlign.value, 'center', 'Button has incorrect text-align');
-  })
+    it('Text - Specify email and password - appears when trying to login with empty password field', function(){
+       const elem = $('#pass');
+        const value = elem.getValue();
+        if (value === '') {
+            browser.click('#login')
+        }
+        let result = browser.waitForVisible('.alert-danger', 5000);
+        assert.equal(result, true, 'The text doesn/t appear when field password is empty');
+    })
 
- it('click registration button', function(){
-    browser.url('/');
-    browser.pause(5000);
-    browser.click('#registration');
-    let result = browser.waitForVisible('button=< Back', 5000);
-    assert.equal(result, true, 'Button Registration is not clicked');
-  })
+
+    it('error design font-family login-page', function(){
+        browser.url('/');
+        browser.waitForVisible('#login', 5000);
+        browser.click('#login');
+        browser.waitForVisible('.alert-local', 5000);
+        let elem = $('.alert-local');
+        let font = elem.getCssProperty('font-family');
+        assert.equal(font.value, 'segoe ui', 'Button has incorrect font-family');
+    })
+
+    it('error design background-color login-page', function(){
+        let elem = $('.alert-local');
+        let background = elem.getCssProperty('background-color');
+        assert.equal(background.parsed.hex, '#f8d7da', 'Button has incorrect background color');
+    })
+
+    it('error design border color login-page', function (start, end){
+        let elem = $('.alert-local');
+        let borderColor = elem.getCssProperty('border');
+        let borderValue = borderColor.value;
+        let result = borderValue.substring(borderValue.indexOf('('));
+        assert.equal(result, '(245, 198, 203)', 'Button has incorrect border color');
+    })
+
+    it('error design font-size login-page', function(){
+        let elem = $('.alert-local');
+        let fontSize = elem.getCssProperty('font-size');
+        assert.equal(fontSize.value, '16px', 'Button has incorrect font-size');
+    })
+
+    it('error design font-weight login-page', function(){
+        let elem = $('.alert-local');
+        let fontWeight = elem.getCssProperty('font-weight');
+        assert.equal(fontWeight.value, '400', 'Button has incorrect font-weight');
+    })
+
+    it('error design color login-page', function(){
+        let elem = $('.alert-danger');
+        let color = elem.getCssProperty('color');
+        assert.equal(color.parsed.hex, '#721c24', 'Button has incorrect color');
+    })
+
+    it('error design text-align login-page', function(){
+        let elem = $('.alert-danger');
+        let textAlign = elem.getCssProperty('text-align');
+        assert.equal(textAlign.value, 'center', 'Button has incorrect text-align');
+    })
+
+    it('click registration button', function(){
+        browser.url('/');
+        browser.pause(5000);
+        browser.click('#registration');
+        let result = browser.waitForVisible('button=< Back', 5000);
+        assert.equal(result, true, 'Button Registration is not clicked');
+    })
 
 });
