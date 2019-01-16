67a54c7ae022adc4ddce931b04c37311d3cbaa36
Victoria Kriuchkova <kriuchkovavictoria@gmail.com>
2019-01-15 18:15:51 -0800

    report script plus allure-commandline module

---
+++ test/loading-screen-victoria-k.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+describe('Client', function () {
+
+    it('Page level title', function(){
+        browser.url('/');
+        assert.equal(browser.getTitle(), 'Bug Tracker');
+    })
+
+    it('Global Header exists', function(){
+        assert.equal($(".custom-header").isVisible(), true);
+    })
+
+});
