2076ceb883f1117c8a3f77c441afaa9fcc162172
asyashi <asia.dudnik@gmail.com>
2019-01-23 22:23:27 -0800

    Updated test cases

--- test/bug-report-form.js
+++ test/bug-report-form.js
@@ -2,35 +2,40 @@ const { assert } = require('chai');
 const exd = require('./../test-data/expected').bugReportForm;
 const gSel = require('./../test-data/selectors');
 const loginSel = gSel.loginFunctionality;
-const bugSel = gSel.bugReportForm;
+const bugSel = gSel.bugList;
+const formSel = gSel.bugReportForm;
 const user = require('./../test-data/users');
 const value = 'test';
 
-describe('Bug report form', function () {
+describe(exd.suite, function () {
 
     it('Verify that when submitting new bug report, Bug Report Form for just created bug will open automatically.', function(){
 
         browser.url('/');
-        browser.waitForVisible(sel.email);
-        browser.setValue(sel.email, user.email);
+        browser.waitForVisible(loginSel.email);
+        browser.setValue(loginSel.email, user.email);
         // browser.pause(2000);
-        browser.setValue(sel.pass, user.pass);
-        browser.click(sel.loginBtn);
+        browser.setValue(loginSel.pass, user.pass);
+        browser.click(loginSel.loginBtn);
         // browser.pause(3000);
-        browser.waitForVisible('#new_bug');
-        browser.click('#new_bug');
+        browser.waitForVisible(bugSel.newBug);
+        browser.click(bugSel.newBug);
         // browser.pause(2000);
+        browser.waitForVisible(formSel.summary);
+        browser.setValue(formSel.summary, value);
+        browser.setValue(formSel.str, value);
+        browser.setValue(formSel.actual, value);
+        browser.setValue(formSel.expected, value);
 
-        browser.setValue('.form-control', value);
-        browser.setValue('.form-control', value);
+        browser.selectByValue(formSel.assignee, 'Anastasia Shibanova');
 
-        browser.selectByValue('placeholder =Assignee *', 'Anastasia Shibanova');
-        browser.selectByValue('=Priority *', 'Low');
-        browser.selectByValue('=Severity *', 'Minor');
-        browser.selectByValue('=Repro *', 'Always');
-        browser.selectByValue('=Version', '0.2');
-        browser.click('#todo_add');
-        // browser.pause(5000);
+        /*browser.selectByValue(formSel.assignee, 'Anastasia Shibanova');
+        browser.selectByValue(formSel.priority, 'Low');
+        browser.selectByValue(formSel.severity, 'Minor');
+        browser.selectByValue(formSel.repro, 'Always');
+        browser.selectByValue(formSel.version, '0.2');
+        // browser.click(formSel.submit);
+        browser.pause(5000);*/
         let edit = browser.isVisible('=Edit');
 
         assert.equal(browser.isVisible('=Edit'), true, 'You are not on Bug report form');
