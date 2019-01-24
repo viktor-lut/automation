fdeced9a01ddf774b79a3914fbab418094b2e340
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-01-24 15:35:18 +0300

    fixed bug in 3 test

--- test/f-password-func.js
+++ test/f-password-func.js
@@ -5,16 +5,15 @@ describe('forgot password functionality', function () {
 
     it('Remind Password', function(){
         browser.url('/');
-        browser.waitForVisible(".btn-link", 5000);
+        browser.waitForVisible(".btn-link", 10000);
 
         $(".btn-link").click();
         browser.waitForVisible("#email", 5000);
 
         $("#email").setValue(['\uE01A', '\uE003']);
-
         $("#remind").click();
 
-        browser.waitForVisible(".alert-danger", 5000);
+        browser.waitForVisible("div*=Please", 5000);
         const danger = $(".alert-danger").getText();
         assert.equal(danger, 'Please specify an email registered in the system');
     });
@@ -22,25 +21,22 @@ describe('forgot password functionality', function () {
     it('email is not registered in the system', function(){
 
         $("#email").setValue('zolotokrad@mail.ru');
-
         $("#remind").click();
-
-        browser.waitForVisible(".alert-danger", 5000);
-        browser.pause(2000);
+        browser.waitForVisible("div*=User", 5000);
         const danger = $(".alert-danger").getText();
         assert.equal(danger, 'User with this email is not registered in the system');
     });
 
+
     it('correct email', function(){
-
+        $(".btn").click();
+        browser.waitForVisible(".btn-link", 5000);
+        $(".btn-link").click();
+        browser.waitForVisible("#email", 5000);
         $("#email").setValue('zolotokrad@gmail.com');
-
         $("#remind").click();
-
         browser.waitForVisible(".alert-danger", 5000);
-        browser.pause(2000);
         const danger = $(".alert-danger").getText();
-        console.log(danger);
         assert.equal(danger, 'Password reminder sent to your email. Please, check your inbox.');
     })
 
