6527d4c7e0834686be83a4d6e3902465ca9d0acd
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-01-24 20:30:34 +0300

    finished writing a test

--- test/f-password-func.js
+++ test/f-password-func.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 //const sel = require('./../test-data/selectors').client;
 
-describe('forgot password functionality', function () {
+describe('General', function () {
 
     it('Remind Password', function(){
         browser.url('/');
@@ -20,7 +20,7 @@ describe('forgot password functionality', function () {
 
     it('email is not registered in the system', function(){
 
-        $("#email").setValue('zolotokrad@mail.ru');
+        $("#email").setValue('takogoadresanesushestvuet@mail.ru');
         $("#remind").click();
         browser.waitForVisible("div*=User", 5000);
         const danger = $(".alert-danger").getText();
@@ -38,12 +38,62 @@ describe('forgot password functionality', function () {
         browser.waitForVisible(".alert-danger", 5000);
         const danger = $(".alert-danger").getText();
         assert.equal(danger, 'Password reminder sent to your email. Please, check your inbox.');
+    });
+
+    it('Verify Message disappears after 5 seconds', function(){
+        $(".btn").click();
+        browser.waitForVisible(".btn-link", 5000);                       //этот
+        $(".btn-link").click();                                    //тест
+        browser.waitForVisible("#email", 5000);                              //написан
+        $("#email").setValue('zolotokrad@gmail.com');                  //не
+        $("#remind").click();                                            //верно
+        const mainPage = browser.waitForVisible(".btn-link", 10000); // а именно эта строка, нужно узнать сколько
+                                                                   // по времени висит надпись 'Password reminder sent to your email.'
+        assert.equal(mainPage, true,'Can not return main page');
     })
 
-
-
-
-
-
+    //////////////////////////////////////////////////////////////////////////
 });
-//zolotokrad@mail.ru
+
+describe('Messages design', function () {
+
+    it('Verify Background color', function(){
+        $(".btn-link").click();
+        browser.waitForVisible("#email", 5000);
+        $("#email").setValue(['\uE01A', '\uE003']);
+        $("#remind").click();
+
+        let backColor = $('.alert-danger').getCssProperty('background-color');
+        assert.equal(backColor.value,'rgba(248,215,218,1)', 'Background color is incorrect');
+    });
+
+    it('Verify border color', function(){
+        let borderColor = $('.alert-danger').getCssProperty('border-top-color');
+        assert.equal(borderColor.value,'rgba(245,198,203,1)', 'Border color is incorrect');
+    });
+
+    it('Verify Font family', function(){
+        let borderColor = $('.alert-danger').getCssProperty('font-family');
+        assert.equal(borderColor.value,'segoe ui', 'Font family is incorrect');
+    });
+
+    it('Verify font size', function(){
+        let fontSize = $('.alert-danger').getCssProperty('font-size');
+        assert.equal(fontSize.value,'16px', 'Font size is incorrect');
+    });
+
+    it('Verify font weight', function(){
+        let fontWeight = $('.alert-danger').getCssProperty('font-weight');
+        assert.equal(fontWeight.value,400, 'Font weight is incorrect');
+    });
+
+    it('Verify font color', function(){
+        let fontColor = $('.alert-danger').getCssProperty('color');
+        assert.equal(fontColor.value,'rgba(114,28,36,1)', 'Font color is incorrect');
+    });
+
+    it('Verify Text is center aligned.', function(){
+        let txtCtrAlign = $('.alert-danger').getCssProperty('text-align');
+        assert.equal(txtCtrAlign.value,'center', 'Text align is incorrect');
+    })
+});
