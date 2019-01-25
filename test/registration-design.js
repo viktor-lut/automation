9f713b1cf63eedfaa4cb4d5f934073d87aa69b01
Yuliya Zavarskaya <yulia@Yuliyas-MacBook-Air.local>
2019-01-25 00:19:47 -0800

    created verificationPlaceholders.js in the helpers folder

--- test/registration-design.js
+++ test/registration-design.js
@@ -5,62 +5,18 @@ const selLoginPage = require("./../test-data/selectors").loginFunctionality;
 const verificationCssValue = require("./../helpers/verificationCssValue");
 const verificationMaxLength = require("./../helpers/verificationMaxLength");
 const verificationHorizontalAligned = require("./../helpers/verificationHorizontalAligned");
+const verificationPlaceholder = require("./../helpers/verificationPlaceholder");
 let btnNames = [];
 let placeholders = [];
 let text = [];
-
-/*function verificationCssValue(selector, cssProp) {
-    const keys = Object.keys(cssProp);
-    let state = "";
-    let value;
-    for (let key of keys) {
-        it(key, function(){
-            switch (key) {
-                case "hover":
-                    state = key + " ";
-                    browser.moveToObject(selector);
-                    browser.pause(200);
-                    cssProp = cssProp[key];
-                    key = Object.keys(cssProp)[0];
-                    break;
-                case "focus":
-                    browser.click(selector);
-                    browser.pause(200);
-                    cssProp = cssProp[key];
-                    key = Object.keys(cssProp)[0];
-                    break;
-            }
-            value = $(selector).getCssProperty(key);
-            value = key.includes("color") ? value["parsed"]["hex"] : value["value"];
-            assert.equal(value, cssProp[key], "expected " + state + key + " is " + cssProp[key] + ", got " + value);
-        })
-    }
-}*/
-
-/*function verificationMaxLength(selector, expMaxLen) {
-    it("max length", function(){
-        const actualMaxLength = $(selector).getAttribute("maxlength");
-        assert.equal(actualMaxLength, expMaxLen, "expected " + expMaxLen + ", got " + actualMaxLength);
-    })
-}*/
+let inputId = [];
+let inputTypes = [];
 
 /*function verificationPlaceholder(selector, expPlaceholder) {
     it("placeholder", function(){
         const actualPlaceholder = $(selector).getAttribute("placeholder");
         assert.equal(actualPlaceholder, expPlaceholder, "expected " + expPlaceholder + ", got " + actualPlaceholder);
     })
-}*/
-
-/*function verificationHorizontalAligned(elementSel, wholePageSel, expAligned) {
-    it("aligned", function(){
-        const pageWidth = browser.getElementSize(wholePageSel,"width");
-        const elementWidth = browser.getElementSize(elementSel,"width");
-        const leftMargin = browser.getLocation(elementSel, "x");
-        const rightMargin = pageWidth - elementWidth - leftMargin;
-        const aligned = leftMargin === rightMargin? "middle" : leftMargin < rightMargin? "left" : "right";
-        assert.equal(aligned, expAligned, "expected " + expAligned + "-aligned, got " + aligned + "-aligned;"
-            + " left margin = " + leftMargin + " right margin = " + rightMargin);
-    });
 }*/
 
 describe("Registration page components", function () {
@@ -71,21 +27,33 @@ describe("Registration page components", function () {
         browser.click(selLoginPage.registrationBtn);
         const registrationPageExist = browser.waitForVisible(sel.btnBack, 10000);
         btnNames = $$(sel.buttonsSel).map(function (el) {return el.getText(); });
-        placeholders = $$(sel.inputsSel).map(function (el) {return el.getAttribute("placeholder"); });
+        //placeholders = $$(sel.inputsSel).map(function (el) {return el.getAttribute("placeholder"); });
+        //inputId = $$(sel.inputsSel).map(function (el) {return el.getAttribute("id"); });
+        //inputTypes = $$(sel.inputsSel).map(function (el) {return el.getAttribute("aria-label"); });
         text = $$(sel.textSel).map(function (el) {return el.getText(); });
         assert.isTrue(registrationPageExist, "Registration page not found");
     });
 
     for (let i = 0; i < exd.expBtnNames.length; i ++) {
-        it("button " + exd.expBtnNames[i], function() {
+        /*it("button " + exd.expBtnNames[i], function() {
             assert.isTrue(btnNames.includes(exd.expBtnNames[i]), exd.expBtnNames[i] + " button not found, got " + btnNames[i]);
-        })
+        });*/
+
+        it("button " + exd.expBtnNames[i] + " is visible", function() {
+            assert.isTrue($('button=' + exd.expBtnNames[i]).isVisible(), exd.expBtnNames[i] + " button is not visible");
+        });
+
     }
 
     for (let i = 0; i < exd.expPlaceholders.length; i ++) {
-        it("text field with placeholder " + exd.expPlaceholders[i], function() {
+        it(exd.expInputs[i] + " field is visible", function() {
+            assert.isTrue($(sel.inputId[i]).isVisible(), inputTypes[i] + " text field is not visible");
+        });
+
+        /*it("text field with placeholder " + exd.expPlaceholders[i], function() {
             assert.isTrue(placeholders.includes(exd.expPlaceholders[i]), exd.expPlaceholders[i] + " placeholder not found, got " + placeholders[i]);
-        })
+        })*/
+
     }
 
     for (let i = 0; i < exd.expText.length; i ++) {
@@ -96,8 +64,23 @@ describe("Registration page components", function () {
 
 });
 
-describe("First name field", function () {
+let expectedMaxLength = 0;
+for (let i = 0; i < exd.expInputs.length; i ++) {
 
+    describe(exd.expInputs[i] + " field", function () {
+        expectedMaxLength = exd.expInputs[i].includes("Name") ? exd.expMaxLength[0] : exd.expMaxLength[1];
+        verificationPlaceholder(sel.inputId[i], exd.expPlaceholders[i]);
+        verificationMaxLength(sel.inputId[i], expectedMaxLength);
+        verificationCssValue(sel.inputId[i], exd.inputCssProp);
+
+    });
+}
+
+
+
+/*describe("First name field", function () {
+
+    verificationPlaceholder(sel.firstName, );
     verificationMaxLength(sel.firstName, exd.expMaxLength[0]);
     verificationCssValue(sel.firstName, exd.inputCssProp);
 
@@ -136,7 +119,7 @@ describe("Confirm Password field", function () {
     verificationMaxLength(sel.confPass, exd.expMaxLength[1]);
     verificationCssValue(sel.confPass, exd.inputCssProp);
 
-});
+});*/
 
 describe("Back button", function () {
 
