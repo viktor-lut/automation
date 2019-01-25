6ddb72866420ffe201d71e2307793e1ce8701741
Olga <volhavauchok@gmail.com>
2019-01-24 21:57:29 -0800

    added global directory with footer function

--- test/global-footer.js
+++ test/global-footer.js
@@ -2,8 +2,15 @@ const { assert } = require('chai');
 const exd = require('./../test-data/expected').globalFooter;
 const sel = require('./../test-data/selectors').globalFooter;
 const selCl = require('./../test-data/selectors').client;
+const footer = require('./global/footer');
 
-describe('General', function () {
+describe(exd.suite[0], function () {
+    footer();
+
+
+});
+
+describe(exd.suite[1], function () {
 
     it('Global Footer exists', function () {
         browser.url('/');
@@ -32,14 +39,14 @@ describe('General', function () {
 
 });
 
-describe('Design Main Footer text', function () {
+describe(exd.suite[2], function () {
 
     it('The font-size is 16px', function(){
         browser.url('/');
         assert.equal($(sel.mainFooter).getCssProperty("font-size").value, exd.mainFootCssProp["font-size"], 'The font-size is not 16px');
     });
 
-    it('The font-family in Footer is segoe ui', function(){
+    it('The font-family is segoe ui', function(){
         let mainFooterFam = $(sel.mainFooter).getCssProperty('font-family').value;
         assert.equal(mainFooterFam, exd.mainFootCssProp["font-family"], 'The font-family is not segoe');
     });
@@ -61,30 +68,30 @@ describe('Design Main Footer text', function () {
 
 });
 
-describe('Design Small Footer text', function () {
+describe(exd.suite[3], function () {
 
-    it('The Small Footer text-align is center', function(){
+    it('The text-align is center', function(){
         browser.url('/');
         let smallFooterAlign = $(sel.smallFooter).getCssProperty('text-align').value;
         assert.equal(smallFooterAlign, exd.smallFootCssProp["text-align"], 'The Small Footer text-align is not center');
     });
 
-    it('The Small Footer font-size is 12px', function(){
+    it('The font-size is 12px', function(){
         let smallFooterSize = $(sel.smallFooter).getCssProperty('font-size').value;
         assert.equal(smallFooterSize, exd.smallFootCssProp["font-size"], 'The Small Footer text-align is not 12px');
     });
 
-    it('The Small Footer font-family is segoe ui', function(){
+    it('The font-family is segoe ui', function(){
         let smallFooterFam = $(sel.smallFooter).getCssProperty('font-family').value;
         assert.equal(smallFooterFam, exd.smallFootCssProp["font-family"], 'The Small Footer font-family is not  segoe ui');
     });
 
-    it('The Small Footer font weight is 400', function(){
+    it('The font weight is 400', function(){
         let smallFooterWeight = $(sel.smallFooter).getCssProperty('font-weight').value;
         assert.equal(smallFooterWeight, exd.smallFootCssProp["font-weight"], 'The Small Footer font is not 400');
     });
 
-    it('The Small Footer color is #212529', function(){
+    it('The color is #212529', function(){
         let smallFooterColor = $(sel.smallFooter).getCssProperty('color').parsed.hex;
         assert.equal(smallFooterColor, exd.smallFootCssProp.color, 'The Small Footer color is not #212529');
     });
