27ab9d39d839c51acfefb9a63f3290b827fed991
Arthur Senko <arthursenko@gmail.com>
2019-02-04 23:58:09 -0800

    removed extra helpers

--- helpers/verificationEmptyField.js
+++
@@ -1,8 +0,0 @@
-const { assert } = require("chai");
-
-module.exports =
-    function (selector, expInput) {
-        it( "field is empty", function() {
-            assert.equal($(selector).getValue(), '', `${expInput} field is not empty`);
-        })
-    };
--- helpers/verificationPlaceholder.js
+++
@@ -1,9 +0,0 @@
-const { assert } = require("chai");
-
-module.exports =
-    function (selector, expPlaceholder) {
-        it("placeholder", function () {
-            const actualPlaceholder = $(selector).getAttribute("placeholder");
-            assert.equal(actualPlaceholder, expPlaceholder, `expected ${expPlaceholder}, got ${actualPlaceholder}`);
-        })
-    };
