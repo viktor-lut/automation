a0af00b713625d9a9d0ccbeb0bf169611d0785a2
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-02-06 21:54:54 +0300

    started write test

--- test/password-reminder-email.js
+++ test/password-reminder-email.js
@@ -2,27 +2,21 @@ const { assert } = require('chai');
 
 describe('General', function () {
 
-    it('Verify receive an email', function(){
+     it('Verify receive an email', function(){
         browser.url('https://mail.google.com');
         browser.waitForVisible("#headingText", 3000);
 
-      //   $(".CwaK9").click();
-        // browser.waitForVisible(sel.email, 5000);
-        //
-        $("#identifierId").setValue("testreacttest@gmail.com");
-        $(".CwaK9").click();
+        $("#identifierId").setValue("bonsecavek@gmail.com");
+        const next = $(".CwaK9");
+        next.click();
         browser.waitForVisible("#headingText");
-        browser.pause(5000);
-        const pass = $("input.whsOnd.zHQkBf");
-        pass.setValue(['t','e','s','t','T','e','s','t']);
         browser.pause(2000);
+        const pass = $("input.whsOnd.zHQkBf");
+        pass.setValue(['T','e','s','t','1','T','e','s','t','2']);
+        browser.pause(3000);
 
         console.log(pass.getValue());
-        $(".CwaK9").click();
-        // $(".VhKI7c").click();
-        //
-        // browser.waitForVisible(sel.emptyField, 5000);
-        // const danger = $(sel.errorMessage).getText();     ".CwaK9" далее
+        next.click();
         browser.pause(1000);
        // assert.equal(danger, exd.remindPass);
     });
