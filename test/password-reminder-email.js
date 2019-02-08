6a0782b410b421ffbb54204ee4a30b3ba4955601
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-02-08 13:39:53 +0300

    just commit

--- test/password-reminder-email.js
+++ test/password-reminder-email.js
@@ -1,24 +1,57 @@
 const { assert } = require('chai');
 
+
 describe('General', function () {
 
      it('Verify receive an email', function(){
+
         browser.url('https://mail.google.com');
         browser.waitForVisible("#headingText", 3000);
-
-        $("#identifierId").setValue("bonsecavek@gmail.com");
+        $("#identifierId").setValue("testreacttest@gmail.com");
         const next = $(".CwaK9");
         next.click();
         browser.waitForVisible("#headingText");
-        browser.pause(2000);
+        browser.pause(1000);
         const pass = $("input.whsOnd.zHQkBf");
-        pass.setValue(['T','e','s','t','1','T','e','s','t','2']);
-        browser.pause(3000);
+        pass.setValue(['t','e','s','t','T','e','s','t']);
+        browser.pause(500);
 
         console.log(pass.getValue());
         next.click();
-        browser.pause(1000);
+
+       // const gmail = $("div[title='Gmail']");
+       // const inbox = $( ".aHS-bnt");
+       // const starred = $( ".J-Ke.n0");
+       // console.log('--*--');
+       // browser.waitForVisible(gmail,9000);
+       // console.log('**-**');
+       // starred.click();
+       // inbox.click();
+       // const nameMail = $("div span [name='react.bug.tracker']");
+       // const mail = $(".zA.zE");
+       // nameMail.click();
+       // browser.pause(1000);
+
        // assert.equal(danger, exd.remindPass);
     });
+
+   it(' it 2', function(){
+
+      const element = $("#gb");
+      const inbox = $( ".aHS-bnt");
+      const starred = $( ".J-Ke.n0");
+      console.log('**before gb**');
+      browser.waitForVisible($(".gb_7d.gb_Ce.gb_fe"),9000);
+      console.log('**after gb**');
+      // starred.click();
+      // inbox.click();
+
+      // const nameMail = $("div span [name='react.bug.tracker']");
+      // const mail = $(".zA.zE");
+      // nameMail.click();
+      // browser.pause(1000);
+
+      // assert.equal(danger, exd.remindPass);
+   });
 
 });
