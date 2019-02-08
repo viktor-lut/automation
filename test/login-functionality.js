e77b76ebdfb36fee7d77b09683e2010ef8116249
ViktoriaKomlik <vica93v@gmail.com>
2019-02-08 15:48:38 -0800

    updated to the origin master

--- test/login-functionality.js
+++ test/login-functionality.js
@@ -1,52 +1,52 @@
 const { assert } = require('chai');
+
 const exd = require('./../test-data/expected').loginFunctionality;
-const sel = require('./../test-data/selectors').loginFunctionality;
-const login = require('./../helpers/login');
+
+const {loginPage, bugList, registration} = require('./../test-data/selectors');
 const randomString = require('./../helpers/randomString');
-const selB = require('./../test-data/selectors').bugList;
-const selR = require('./../test-data/selectors').registrationDesign;
+
 const user = require('./../test-data/users');
 
 describe(exd.suite, function () {
 
     it('Email accepts 1 symbol', function () {
         browser.url('/');
-        browser.waitForVisible(sel.loginBtn, 3000);
-        $(sel.email).setValue(randomString(sel.minS));
-        let result = $(sel.email).getValue();
+        browser.waitForVisible(loginPage.loginBtn, 3000);
+        $(loginPage.email).setValue(randomString(loginPage.minS));
+        let result = $(loginPage.email).getValue();
         assert.equal(result.length, exd.expMinLength, 'Email field doesn/t accepts 1 letter');
     });
 
     it('Email accepts 45 symbols', function(){
-         browser.clearElement(sel.email);
-         $(sel.email).setValue(randomString(sel.maxS));
-         let result = $(sel.email).getValue();
+         browser.clearElement(loginPage.email);
+         $(loginPage.email).setValue(randomString(loginPage.maxS));
+         let result = $(loginPage.email).getValue();
          assert.equal(result.length, exd.expMaxLength, 'Email field  doesn/t accept 45 symbols');
      });
 
      it('Field Email doesn/t accepts 46 symbols', function(){
-         browser.clearElement(sel.email);
-         $(sel.email).setValue(randomString(sel.maxS+1));
-         let result = $(sel.email).getValue();
+         browser.clearElement(loginPage.email);
+         $(loginPage.email).setValue(randomString(loginPage.maxS+1));
+         let result = $(loginPage.email).getValue();
          assert.equal(result.length, exd.expMaxLength, 'Email field doesn/t accept 46 symbols')
 
      });
 
      it('Text - Specify email and password - when login is empty', function(){
-         $(sel.email).setValue(['a', '\uE003']);
-         $(sel.pass).setValue(randomString(exd.expMinLength));
-         browser.click(sel.loginBtn);
-         browser.waitForVisible(sel.loginBtn, 3000);
-         assert.isTrue($(sel.alertSps).isVisible());
+         $(loginPage.email).setValue(['a', '\uE003']);
+         $(loginPage.pass).setValue(randomString(exd.expMinLength));
+         browser.click(loginPage.loginBtn);
+         browser.waitForVisible(loginPage.loginBtn, 3000);
+         assert.isTrue($(loginPage.alertSps).isVisible());
     });
 
     it('Text - Specify email and password - when login is wrong', function(){
-        browser.clearElement(sel.email);
-        browser.clearElement(sel.pass);
-        browser.setValue(sel.email, user.email +'a');
-        browser.setValue(sel.pass, user.pass);
-        browser.click(sel.loginBtn);
-        assert.isTrue(browser.waitForVisible(sel.alertDntEx,5000));
+        browser.clearElement(loginPage.email);
+        browser.clearElement(loginPage.pass);
+        browser.setValue(loginPage.email, user.email +'a');
+        browser.setValue(loginPage.pass, user.pass);
+        browser.click(loginPage.loginBtn);
+        assert.isTrue(browser.waitForVisible(loginPage.alertDntEx,5000));
     });
 });
 
@@ -54,44 +54,44 @@ describe(exd.suite, function () {
 
      it('Password accepts 1 symbol', function () {
          browser.url('/');
-         browser.waitForVisible(sel.loginBtn, 3000);
-         $(sel.pass).setValue(randomString(sel.minS));
-         let result = $(sel.pass).getValue();
+         browser.waitForVisible(loginPage.loginBtn, 3000);
+         $(loginPage.pass).setValue(randomString(loginPage.minS));
+         let result = $(loginPage.pass).getValue();
          assert.equal(result.length, exd.expMinLength, 'Password field doesn/t accepts 1 letter');
      });
 
      it('Password accepts 45 symbols', function(){
-         browser.clearElement(sel.pass);
-         $(sel.pass).setValue(randomString(sel.maxS));
-         let result = $(sel.pass).getValue();
+         browser.clearElement(loginPage.pass);
+         $(loginPage.pass).setValue(randomString(loginPage.maxS));
+         let result = $(loginPage.pass).getValue();
          assert.equal(result.length, exd.expMaxLength, 'Password field  doesn/t accept 45 symbols');
      });
 
      it('Password doesn/t accepts 46 symbols', function(){
-         browser.clearElement(sel.pass);
-         $(sel.pass).setValue(randomString(sel.maxS+1));
-         let result = $(sel.pass).getValue();
+         browser.clearElement(loginPage.pass);
+         $(loginPage.pass).setValue(randomString(loginPage.maxS+1));
+         let result = $(loginPage.pass).getValue();
          assert.equal(result.length, exd.expMaxLength, 'Password field doesn/t accept 46 symbols');
      });
 
      it('Text - Specify email and password - when password is empty', function(){
-         $(sel.email).setValue(randomString(exd.expMinLength));
-         $(sel.email).setValue(['a', '\uE003']);
-         browser.click(sel.loginBtn);
-         assert.isTrue(browser.waitForVisible(sel.alertSps, 3000));
+         $(loginPage.email).setValue(randomString(exd.expMinLength));
+         $(loginPage.email).setValue(['a', '\uE003']);
+         browser.click(loginPage.loginBtn);
+         assert.isTrue(browser.waitForVisible(loginPage.alertSps, 3000));
      });
 
      it('Text - Specify email and password - when password is wrong', function(){
-         browser.clearElement(sel.email);
-         browser.clearElement(sel.pass);
-         browser.setValue(sel.email, user.email);
-         browser.setValue(sel.pass, user.pass + 'a');
-         browser.click(sel.loginBtn);
-         assert.isTrue(browser.waitForVisible(sel.alertPassIncrt, 3000));
+         browser.clearElement(loginPage.email);
+         browser.clearElement(loginPage.pass);
+         browser.setValue(loginPage.email, user.email);
+         browser.setValue(loginPage.pass, user.pass + 'a');
+         browser.click(loginPage.loginBtn);
+         assert.isTrue(browser.waitForVisible(loginPage.alertPassIncrt, 3000));
      });
 
      it('Symbols in password field are replaced by bullets', function () {
-         let result = $(sel.pass).getCssProperty('-webkit-text-security');
+         let result = $(loginPage.pass).getCssProperty('-webkit-text-security');
          assert.equal(result.property, exd.bulletsPass, 'Symbols in password are not replaced by bullets.');
      });
  });
@@ -100,42 +100,42 @@ describe('Error design', function () {
 
    it('error font-family login-page', function(){
         browser.url('/');
-        browser.waitForVisible(sel.loginBtn, 3000);
-        browser.click(sel.loginBtn);
-        browser.waitForVisible(sel.alert, 3000);
-        let font = $(sel.alert).getCssProperty('font-family');
+        browser.waitForVisible(loginPage.loginBtn, 3000);
+        browser.click(loginPage.loginBtn);
+        browser.waitForVisible(loginPage.alert, 3000);
+        let font = $(loginPage.alert).getCssProperty('font-family');
         assert.equal(font.value, exd.errorCss["font-family"], 'Button has incorrect font-family');
     });
 
     it('error background-color login-page', function(){
-        let background =  $(sel.alert).getCssProperty('background-color');
+        let background =  $(loginPage.alert).getCssProperty('background-color');
         assert.equal(background.parsed.hex, exd.errorCss["background-color"], 'Button has incorrect background color');
     });
 
     it('error border color login-page', function (){
-        let borderColor = $(sel.alert).getCssProperty('border');
+        let borderColor = $(loginPage.alert).getCssProperty('border');
         let borderValue = borderColor.value;
         let result = borderValue.substring(borderValue.indexOf('('));
         assert.equal(result, exd.errorCss.border, 'Button has incorrect border color');
     });
 
     it('error font-size login-page', function(){
-        let fontSize = $(sel.alert).getCssProperty('font-size');
+        let fontSize = $(loginPage.alert).getCssProperty('font-size');
         assert.equal(fontSize.value, exd.errorCss.fontSize, 'Button has incorrect font-size');
     });
 
     it('error  font-weight login-page', function(){
-        let fontWeight = $(sel.alert).getCssProperty('font-weight');
+        let fontWeight = $(loginPage.alert).getCssProperty('font-weight');
         assert.equal(fontWeight.value, exd.errorCss["font-weight"], 'Button has incorrect font-weight');
     });
 
     it('error design color login-page', function(){
-        let color = $(sel.alert).getCssProperty('color');
+        let color = $(loginPage.alert).getCssProperty('color');
         assert.equal(color.parsed.hex, exd.errorCss.color, 'Button has incorrect color');
     });
 
     it('error design text-align login-page', function(){
-        let textAlign = $(sel.alert).getCssProperty('text-align');
+        let textAlign = $(loginPage.alert).getCssProperty('text-align');
         assert.equal(textAlign.value, exd.errorCss["text-align"], 'Button has incorrect text-align');
     });
 });
@@ -143,19 +143,19 @@ describe('Error design', function () {
 describe('Correct credentials', function () {
     it('Log into the system', function(){
         browser.url('/');
-        browser.waitForVisible(sel.email);
-        browser.setValue(sel.email, user.email);
-        browser.setValue(sel.pass, user.pass);
-        browser.click(sel.loginBtn);
-        assert.isTrue(browser.waitForVisible(selB.newBug));
+        browser.waitForVisible(loginPage.email);
+        browser.setValue(loginPage.email, user.email);
+        browser.setValue(loginPage.pass, user.pass);
+        browser.click(loginPage.loginBtn);
+        assert.isTrue(browser.waitForVisible(bugList.newBug));
     });
 });
 
 describe('Registration page exists', function () {
     it('Registration button Clickable and redirects to the right page', function(){
         browser.url('/');
-        browser.waitForVisible(sel.registrationBtn, 3000);
-        browser.click(sel.registrationBtn);
-        assert.equal(browser.waitForVisible(sel.btnBack, 1500), true, 'Button Registration is not clicked');
+        browser.waitForVisible(loginPage.registrationBtn, 3000);
+        browser.click(loginPage.registrationBtn);
+        assert.equal(browser.waitForVisible(registration.buttonIds.Back, 1500), true, 'Button Registration is not clicked');
     })
 });
