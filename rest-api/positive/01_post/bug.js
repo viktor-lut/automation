1991aa0d33ffc558f6cc6e89e64668ef3cb65aba
Arthur Senko <arthursenko@gmail.com>
2019-02-10 20:42:38 -0800

    api refactor

--- rest-api/positive/01_post/bug.js
+++ rest-api/positive/01_post/bug.js
@@ -2,22 +2,19 @@ const { postBug } = require('../../../helpers/axios');
 const { assert } = require('chai');
 const { bug, postResMessage } = require('../../../test-data/api');
 const fs = require('fs');
-const id = require('./../../../test-data/id');
 
 describe('POST', () => {
   it('Positive', () => {
     return postBug()
       .then(res => {
-        id.id = res.bug._id;
-        assert.deepEqual(res.bug.bug, bug.bug);
-      })
-      .then(() => {
         fs.writeFile(`${__dirname}/../../../test-data/id.json`,
-          JSON.stringify(id),
+          JSON.stringify(res.bug._id),
           'utf8',
           function (err) {
             if (err) throw err;
           });
+        assert.equal(res.message, postResMessage);
+        assert.deepEqual(res.bug.bug, bug.bug);
       })
   });
 });
