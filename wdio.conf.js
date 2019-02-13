bdd88080ecafa21f824ecdfbaee608d75c3d9159
Artur Senko <asenko@apple.com>
2019-02-12 19:40:23 -0800

    fixed remove

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,16 +1,16 @@
 exports.config = {
 
   specs: [
-    './test/login-page-design.js',
-    './test/client.js',
-    './test/f-password-func.js',
-    './test/login-functionality.js',
-    './test/registration-design.js',
-    './test/registration-functionality.js',
-    './test/new-bug-report.js',
-    './test/global-footer.js',
-    './test/navigation-bar.js',
-    './test/bug-form-page-validation.js',
+    // './test/login-page-design.js',
+    // './test/client.js',
+    // './test/f-password-func.js',
+    // './test/login-functionality.js',
+    // './test/registration-design.js',
+    // './test/registration-functionality.js',
+    // './test/new-bug-report.js',
+    // './test/global-footer.js',
+    // './test/navigation-bar.js',
+    // './test/bug-form-page-validation.js',
     './rest-api/**/**/*.js'
   ],
 
